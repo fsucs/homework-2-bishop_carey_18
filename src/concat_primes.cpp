23acf222a2ed25e4f6bb5b86920f65863ef9be17
alicebishop <77136565+alicebishop@users.noreply.github.com>
2021-01-20 01:45:34 -0500

    Update concat_primes.cpp

--- src/concat_primes.cpp
+++ src/concat_primes.cpp
@@ -1,17 +1,24 @@
 /** 
    Project 2: Concatenated Primes
 
-   Author:Alice Bishop and Kathryn Carey
+   Author: Alice Bishop and Kathryn Carey
 
    This program asks the use to input a number. It then returns the the
    prime numbers to five places from the inputted number.
 */
 
 /** \file concat_primes.cpp
-    \brief Brief explanation...
+    program returns a string of the first 5 numbers starting at a user inputed index
     
-    Long explination goes here...
-
+    In this program, the user will input an int n. While n is being read in, 
+    the function get_concatenated_primes() is called. This function finds all the prime numbers starting at 2 and ends at 2000.
+    Once a prime number is found, it will turn the int into a string and concate it onto the concat_primes string.
+    After it finds all the prime numbers between 2 and 2000, the function will shorten the string to a length of 1000 using substr and returns it to main. 
+    The string returned is set equal to the string concat_primes. The function get_slice_of_5 is called next. 
+    It uses concat_primes and the user input int n as the parameters. In the get_slice_of_5 function, substr is used again, index is where it will start
+    and it will read the next 5 numbers. It is set equal to the ret string, and ret is returned. In the main, when ret is returned it is printed on the screen using cout.
+    
+ 
     Requires: C++11
 */
 
@@ -20,6 +27,7 @@
 #include<string>
 #include<assert.h>
 #include<boost/lexical_cast.hpp>
+
 /** \brief Brief function explaination
    
   
